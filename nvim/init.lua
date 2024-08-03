021b3f6ce46052cbd4b919bcfd5ec94052ebe855
Hugo Vazquez <hugo110795@gmail.com>
2024-08-03 14:11:02 -0600

    Updating dotfiles for nvim and keymaps

--- nvim/init.lua
+++ nvim/init.lua
@@ -8,3 +8,5 @@ vim.opt.cursorline = true
 vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
+vim.opt.scrolloff = 20
+vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,4 +1,10 @@
 return {
   'nvim-telescope/telescope.nvim', tag = '0.1.8',
-  dependencies = { 'nvim-lua/plenary.nvim' }
+  dependencies = { 'nvim-lua/plenary.nvim' },
+  opts = {
+    defaults = {
+      wrap_results = false,
+      layout_strategy = 'center'
+    }
+  }
 }
