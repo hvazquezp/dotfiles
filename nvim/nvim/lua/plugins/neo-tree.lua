6a09a2548060c123be688c411c43da0685ec1af5
Hugo Vazquez <hugo110795@gmail.com>
2024-08-24 00:41:45 -0600

    Adding the ignore hidden

--- nvim/nvim/lua/plugins/neo-tree.lua
+++ nvim/nvim/lua/plugins/neo-tree.lua
@@ -9,6 +9,11 @@ return {
     config = function ()
       require('neo-tree').setup({
         filesystem = {
+          filtered_items = {
+            visible = true,
+            hide_dotfiles = false,
+            hide_gitignored = true
+          },
           hijack_netrw_behavior = "open_current",
         }
 
