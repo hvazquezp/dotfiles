51de08f929ed40f2af2b59a6bceb16047fe4903d
Hugo Vazquez <hugo110795@gmail.com>
2024-08-24 00:38:47 -0600

    Adding changes to the dotfiles

--- nvim/nvim/init.lua
+++ nvim/nvim/init.lua
@@ -3,7 +3,7 @@ vim.g.loaded_netrwPlugin = 1
 vim.g.mapleader = ' '
 require("config.lazy")
 require("plugin_config")
-vim.opt.number = true
+vim.opt.relativenumber = true
 vim.opt.showcmd = true
 vim.opt.cursorline = true
 
