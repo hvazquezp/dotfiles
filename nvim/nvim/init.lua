60a42202bb7baf6b13098606ecf2ac63e8b79234
Hugo Vazquez <hugo110795@gmail.com>
2024-09-02 12:34:44 -0600

    Adding small changes

--- nvim/nvim/init.lua
+++ nvim/nvim/init.lua
@@ -4,6 +4,7 @@ vim.g.mapleader = ' '
 require("config.lazy")
 require("plugin_config")
 vim.opt.relativenumber = true
+vim.opt.number = true
 vim.opt.showcmd = true
 vim.opt.cursorline = true
 
--- nvim/nvim/lua/plugins/lualine.lua
+++ nvim/nvim/lua/plugins/lualine.lua
@@ -6,7 +6,21 @@ return {
       require('lualine').setup({
         options = {
           -- theme = 'horizon'
-          theme = 'modus-vivendi'
+          theme = 'modus-vivendi',
+          component_separators = { left = '|', right = '|' }
+        },
+        sections = {
+          lualine_a = {'mode'},
+          lualine_b = {'branch', 'diff', 'diagnostics'},
+          lualine_c = {{
+            'buffers'
+          }},
+          lualine_y = {'progress'},
+          lualine_x = {'searchcount',  {
+            'filename',
+            path = 1,
+          } , 'filetype'},
+          lualine_z = {'location'}
         }
       })
     end
